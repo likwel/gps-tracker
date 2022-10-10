6f40f6968604108cf3f95d05ac58d85f50a0dba8
Elie <eliefenohasina@gmail.com>
2022-10-10 16:32:05 +0300

    ajout icon

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 //Initialisation carte map
 
-var map = L.map('map', { zoomControl: false }).setView([-18.8724342 , 47.5177325 ], 6);
+var map = L.map('map', { zoomControl: false }).setView([-18.8724342, 47.5177325], 5);
 
 var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     //attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -32,78 +32,96 @@ var baseLayers = {
 
 L.control.layers(baseLayers).addTo(map);
 
-L.control.scale({ position: 'bottomright',imperial :false }).addTo(map);
+L.control.scale({ position: 'bottomright', imperial: false }).addTo(map);
 L.control.zoom({ position: 'bottomright' }).addTo(map);
 
-/*
-if (!navigator.geolocation) {
-    console.log("Your browser doesn't support geolocation feature!")
-} else {
-    setInterval(() => {
-        navigator.geolocation.getCurrentPosition(getPosition)
-        
-    }, 5000);
+var Lat, Long;
+var datas = [];
+var tab_marker = [];
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+
+        Lat = position.coords.latitude;
+        Long = position.coords.longitude;
+        var latlng = getGeoLocation(Lat, Long);
+
+        setInterval(() => {
+
+            datas.push([Lat, Long]);
+            traceLine(datas);
+
+            var singleMarker = setMarker(Lat, Long, "1589 TBH");
+
+            map.setView([Lat, Long], 5);
+
+            tab_marker.push(singleMarker);
+
+            for (i = 1; i < tab_marker.length - 1; i++) {
+                map.removeLayer(tab_marker[i]);
+                //tab[i].addTo(map);
+            }
+            Lat += 0.5;
+            Long *= 0.5;
+
+        }, 1000);
+
+
+    }, function () {
+        //error
+    });
 }
-*/
 
-var marker, circle;
-
-function getPosition(position) {
-    // console.log(position)
-    var lat = position.coords.latitude
-    var long = position.coords.longitude
-    var accuracy = position.coords.accuracy
-
-    if (marker) {
-        map.removeLayer(marker)
-    }
-
-    if (circle) {
-        map.removeLayer(circle)
-    }
-
-    marker = L.marker([lat, long])
-    circle = L.circle([lat, long], { radius: accuracy })
-
-    var featureGroup = L.featureGroup([marker, circle]).addTo(map)
-
-    map.fitBounds(featureGroup.getBounds())
-
-    console.log("Your coordinate is: Lat: " + lat + " Long: " + long + " Accuracy: " + accuracy)
+function getGeoLocation(_lat, _long) {
+    var latlng = { "lat": _lat, "lng": _long };
+    return latlng;
 }
 
-function setMarker(latitude,longitude, titre) {
-    
-    var singleMarker = L.marker([latitude, longitude]);
+
+function setMarker(latitude, longitude, titre) {
+
+    var singleMarker = L.marker([latitude, longitude], { icon: iconCustom });
     singleMarker.addTo(map);
-    let popup = singleMarker.bindPopup('<div style = "margin-bottom:10px;"><strong> '+titre+'</strong></div> <div><i > Latitude : ' + latitude + ' </i></div><br></div> <div><i > Longitude : ' + longitude + ' </i></div>');
+    let popup = singleMarker.bindPopup('<div style = "margin-bottom:10px;"><strong> ' + titre + '</strong></div> <div><i > Latitude : ' + latitude + ' </i></div><br></div> <div><i > Longitude : ' + longitude + ' </i></div>');
     popup.addTo(map);
     singleMarker.on('mouseover', () => popup.openPopup());
     singleMarker.on('mouseout', () => popup.closePopup());
-   
+
+    return singleMarker;
+
 }
 
 function getData() {
-    
+
     var arr = [];
     fetch('http://localhost:3000/show')
         .then(result => result.json())
         .then(result => {
-            var cmpt =0;
-            for (var res of result){
-                if(cmpt>=119){
-                    arr.push([res.latitude,res.longitude]);
+            var cmpt = 0;
+            for (var res of result) {
+                if (cmpt >= 119) {
+                    arr.push([res.latitude, res.longitude]);
                 }
                 cmpt++;
             }
             console.log(arr);
-            setMarker(arr[0][0],arr[0][1], 'Départ');
+            setMarker(arr[0][0], arr[0][1], 'Départ');
 
             traceLine(arr)
-            setMarker(arr[1500][0],arr[1500][1] , '15087 TBL')
-            setMarker(arr[arr.length-1][0],arr[arr.length-1][1] , 'Arrivée');
+            setMarker(arr[1500][0], arr[1500][1], '15087 TBL')
+            setMarker(arr[arr.length - 1][0], arr[arr.length - 1][1], 'Arrivée');
         });
 }
+
+var iconCustom = L.icon({
+    iconUrl: './icon/car.png',
+    iconSize: [25, 25],
+    iconAnchor: [12.5, 25],
+    popupAnchor: [0, -20],
+    //shadowUrl: 'my-icon-shadow.png',
+    shadowSize: [68, 95],
+    shadowAnchor: [22, 94]
+
+});
 
 function traceLine(data) {
     var latlngs = [
@@ -111,51 +129,54 @@ function traceLine(data) {
         [37.77, -122.43],
         [34.04, -118.2]
     ];
-    var polyline = L.polyline(data, { color: 'red', weight: 4, opacity: 0.5,smoothFactor: 1 }).addTo(map);
+    var polyline = L.polyline(data, { color: 'red', weight: 4, opacity: 1, smoothFactor: 1 }).addTo(map);
 
-    // zoom the map to the polyline
     map.fitBounds(polyline.getBounds());
 }
-//traceLine();
-//getData();
 
-function close(className){
-    document.querySelector(className).style.display="inline-block";
-    document.querySelector(".sidenav").style.display="none";
-}
-//close appareil
-close(".appareil");
-//close parametre
-close(".parametre");
-//close notification
-close(".notification");
+function device() {
+    document.querySelector(".appareil").style.display = "inline-block";
+    document.querySelector(".sidenav").style.display = "none";
+    //document.querySelector("#map").style.opacity="0.5";
 
-function el_close(){
-    document.querySelector(".device").style.display="none";
-    document.querySelector("#map").style.opacity="1";
 }
-function create_app(){
-    document.querySelector(".device").style.display="inline-block";
+function parametre() {
+    document.querySelector(".parametre").style.display = "inline-block";
+    document.querySelector(".sidenav").style.display = "none";
+
+}
+function notification() {
+    document.querySelector(".notif").style.display = "inline-block";
+    document.querySelector(".sidenav").style.display = "none";
+
 }
 
-function quit_app(){
-    document.querySelector(".appareil").style.display="none";
-    document.querySelector(".sidenav").style.display="block";
+function el_close() {
+    document.querySelector(".device").style.display = "none";
+    document.querySelector("#map").style.opacity = "1";
+}
+function create_app() {
+    document.querySelector(".device").style.display = "inline-block";
 }
 
-function quit_param(){
-    document.querySelector(".parametre").style.display="none";
-    document.querySelector(".sidenav").style.display="block";
+function quit_app() {
+    document.querySelector(".appareil").style.display = "none";
+    document.querySelector(".sidenav").style.display = "block";
 }
-function quit_notif(){
-    document.querySelector(".notif").style.display="none";
-    document.querySelector(".sidenav").style.display="block";
+
+function quit_param() {
+    document.querySelector(".parametre").style.display = "none";
+    document.querySelector(".sidenav").style.display = "block";
+}
+function quit_notif() {
+    document.querySelector(".notif").style.display = "none";
+    document.querySelector(".sidenav").style.display = "block";
 }
 
 
 var dropdown = document.getElementsByClassName("icon-down");
 var dropdown2 = document.getElementsByClassName("dropdown-btn");
-function showDropDown(dropdown){
+function showDropDown(dropdown) {
     for (var i = 0; i < dropdown.length; i++) {
         dropdown[i].addEventListener("click", function () {
             this.classList.toggle("active");
